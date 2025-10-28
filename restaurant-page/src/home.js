20c421f3db00903326186709369cd8cb699be479
Devolas <darian.dihal@gmail.com>
2025-10-28 19:59:05 -0400

    added bg images to home page

--- restaurant-page/src/home.js
+++ restaurant-page/src/home.js
@@ -1,3 +1,8 @@
+import foodImageImport1 from "./home-food-images/edward-howell-TACZ-gz4li4-unsplash.jpg";
+import foodImageImport2 from "./home-food-images/fernando-andrade-TrD7yA09Vg8-unsplash.jpg";
+import foodImageImport3 from "./home-food-images/vino-li-TqOEGdRNowY-unsplash.jpg";
+
+
 function home() {
     const containerDiv = document.getElementById(`container`); // we have to regrab the containerDiv here because home.js doesn't have access to that variable
     const lowerMain = document.createElement("div");
@@ -18,17 +23,37 @@ function home() {
     imgGrid.setAttribute("class", "img-grid");
     lowerMain.appendChild(imgGrid);
 
+    //////////////////////
+
     const bg1 = document.createElement("div");
     bg1.setAttribute("class", "background-color1");
     imgGrid.appendChild(bg1);
+
+    const foodImg1 = document.createElement("img");
+    foodImg1.src = foodImageImport1;
+    bg1.appendChild(foodImg1);
+
+    ////////
 
     const bg2 = document.createElement("div");
     bg2.setAttribute("class", "background-color2");
     imgGrid.appendChild(bg2);
 
+    const foodImg2 = document.createElement("img");
+    foodImg2.src = foodImageImport2;
+    bg2.appendChild(foodImg2);
+
+    ////////
+
     const bg3 = document.createElement("div");
     bg3.setAttribute("class", "background-color3");
     imgGrid.appendChild(bg3);
+
+    const foodImg3 = document.createElement("img");
+    foodImg3.src = foodImageImport3;
+    bg3.appendChild(foodImg3);
+
+    ////////
 }
 
 export default home;
